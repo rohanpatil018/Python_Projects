8e40f6c1a14c313b5baa6ccad1b1bb92454d04cb
Rohan Patil <148521216+rohanpatil018@users.noreply.github.com>
2025-08-30 11:56:01 +0530

    Add files via upload

---
+++ Guess_The_Number.py
@@ -0,0 +1,18 @@
+import random
+
+
+n=random.randint(1,100)
+
+a=-1
+guesses=0
+
+while(a!=n):
+    guesses+=1
+    a=int(input("Guess a number: "))    
+
+    if(a>n):
+        print("Lower Number please!!")
+    else:
+        print("Higher Number please!!")
+
+print(f'You have correctly gussed the number {n} in {guesses} attempt')
